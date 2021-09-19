b52049154c1ed40d1264504caba85f93c7d83db4
Oli-idk <67001116+Oli-idk@users.noreply.github.com>
2021-09-19 16:16:12 -0700

    fix grammar

--- events/Lavalink/trackError.js
+++ events/Lavalink/trackError.js
@@ -7,7 +7,7 @@ module.exports = async (client, player, track, payload) => {
     const channel = client.channels.cache.get(player.textChannel);
     const thing = new MessageEmbed()
         .setColor("RED")
-        .setDescription("❌ Error when loading song! Track is error");
+        .setDescription("❌ Error when loading song! Track caused an error");
     channel.send({embeds: [thing]});
     client.logger.log(`Error when loading song! Track is error in [${player.guild}]`, "error");
     if (!player.voiceChannel) player.destroy();
